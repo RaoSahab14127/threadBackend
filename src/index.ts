dadf46b8aea38fba49bc58b7db1590b483376ae1
raoSahab14127 <eb20103109.raohammadali@gmail.com>
2024-05-13 04:19:37 -0700

    done

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import express from 'express'
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from "@apollo/server/express4"
+import bodyParser from 'body-parser'
+import cors from 'cors'
+async function starter(){
 
-const app = express()
-const PORT = Number(process.env.PORT) || 8000;
+    const app = express()
+    const PORT = Number(process.env.PORT) || 8000;
+    const gqlServer = new ApolloServer({
+        typeDefs:``,
+        resolvers:{}
+    } )    
+    
+   
+    app.use(bodyParser.json())
+    app.use(cors())
 
+    await gqlServer.start()
+    
+    app.use("/gqlapi", expressMiddleware(gqlServer))
 
-app.get("/", (req, res)=>{
-    res.json({ message:"server is running at / route"})
-})
+    app.get("/", (req, res)=>{
+        res.json({ message:"server is running at / route"})
+    })
+    
+    app.listen(PORT, ()=>{
+        console.log(`server is running at port ${PORT}`)
+    })
 
-app.listen(PORT, ()=>{
-    console.log(`server is running at port ${PORT}`)
-})
+}
+
+starter()
